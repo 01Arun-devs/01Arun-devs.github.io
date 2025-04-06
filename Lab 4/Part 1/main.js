2110377d7036cf317861c388b44fea59861770ef
01Arun-devs <arunkum1904@gmail.com>
2025-04-05 21:16:28 -0400

    Part 3 completed

--- Lab 4/Part 1/main.js
+++ Lab 4/Part 1/main.js
@@ -1,3 +1,9 @@
+
+// Name: Arun Kumar
+// File: main.js
+// Date: March 31, 2025
+// Random story generator
+
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
@@ -13,20 +19,31 @@ const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-
-
 randomize.addEventListener('click', result);
 
 function result() {
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replaceAll(":inserty:", yItem);
+    newStory = newStory.replaceAll(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll("Bob", name);
 
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 * 0.0714286)} stone`;
+    const temperature = `${Math.round((94 - 32) * 5 / 9)} centigrade`;
+
+    newStory = newStory.replaceAll("94 fahrenheit", temperature);
+    newStory = newStory.replaceAll("300 pounds", weight);
 
   }
 
---
+++ Lab 4/Part 2/main.js
@@ -0,0 +1,56 @@
+
+// Name: Arun Kumar
+// File: main.js
+// Date: March 31, 2025
+// Image gallery
+
+
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+/* Declaring the alternative text for each image file */
+
+const obj = {
+    "pic1.jpg": "Picture of Human eye",
+    "pic2.jpg": "Picture of something wavy",
+    "pic3.jpg": "Lavender flowers",
+    "pic4.jpg": "Ancient Egypt painting",
+    "pic5.jpg": "Butterfly image"
+}
+
+/* Looping through images */
+
+for (let image of images) {
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", `images/${image}`);
+    imgElement.setAttribute("alt", obj[image]);
+    thumbBar.appendChild(imgElement);
+
+    imgElement.addEventListener("click", imgEvent => {
+        displayedImage.src = imgEvent.target.src;
+        displayedImage.alt = imgEvent.target.alt;
+    });
+}
+
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener("click", btnEvent => {
+    const btnClass = btn.getAttribute("class");
+    if (btnClass === "dark") {
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    } else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Dark";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+
+    }
+})
